98c7bae9459a264ee82cab1f765437343264a5b0
totsapol_n <totsapol.piluk@gmail.com>
2024-02-18 17:40:44 +0700

    margin

--- src/page/project.jsx
+++ src/page/project.jsx
@@ -1,7 +1,7 @@
 export function Myproject() {
   return (
-    <div id="project" className="flex items-center flex-col h-[100vh] pb-[100px] gap-4 bg-[#f9f9f9] drop-shadow-md" >
-      <h2 className="mt-[100px] text-4xl font-extrabold">MY <span className="text-[#FFE01B]">PROJECT</span></h2>
+    <div id="project" className="flex items-center justify-center flex-col h-[100vh] pb-[100px] gap-4 bg-[#f9f9f9] drop-shadow-md" >
+      <h2 className="text-4xl font-extrabold">MY <span className="text-[#FFE01B]">PROJECT</span></h2>
       <div className="w-[50%] h-[70%] rounded-3xl drop-shadow-2xl flex gap-2 justify-center items-center">
         <span className="w-[280px] h-[380px] flex flex-col items-center">
           <a href="https://loglife.guru/" target="_blank" className="dropCSD">
@@ -10,8 +10,9 @@ export function Myproject() {
               alt="loglife website"
               className=" rounded-t-lg"
             />
-            <p className="py-4 px-[1px] text-md font-semibold bg-white rounded-b-lg border-t-2 text-center">
-              Web appication for activity tracking
+            <p className="py-4 px-[1px] text-md font-semibold bg-white rounded-b-lg border-t-2 text-start pl-4">
+              <span className="text-indigo-500 font-extrabold">PROJECT:</span> Loglife (team project)<br/> 
+              <span className="text-indigo-500 font-extrabold">TOOL:</span> react<span className="text-green-500"> • </span> node js<span className="text-rose-500"> • </span> mongoDB
             </p>
           </a>
         </span>
